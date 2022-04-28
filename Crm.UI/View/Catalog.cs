817e9867bb8a69e239b28eb2511117fddcf82791
IvanSminrov <includingivansmirnov@gmail.com>
2022-04-28 14:44:50 +0500

    1

--- Crm.UI/View/Catalog.cs
+++ Crm.UI/View/Catalog.cs
@@ -11,14 +11,16 @@ namespace Crm.UI.View
         where T : class
     {
 
-        private CrmContext _context; 
+        private CrmContext _context;
+        private DbSet<T> _dbSet;
 
         public Catalog(DbSet<T> dbSet, CrmContext context)
         {
             InitializeComponent();
             _context = context;
-            dbSet.Load();
-            dataGridView.DataSource = dbSet.Local.ToBindingList();
+            _dbSet = dbSet;
+            _dbSet.Load();
+            dataGridView.DataSource = _dbSet.Local.ToBindingList();
         }
 
         private void Catalog_Load(object sender, EventArgs e)
@@ -45,6 +47,24 @@ namespace Crm.UI.View
         private void EditButton_Click(object sender, EventArgs e)
         {
             var id = dataGridView.SelectedRows[0].Cells[0].Value;
+
+            if (typeof(T) == typeof(Product))
+            {
+                var product = _dbSet.Find(id) as Product;
+                if(product != null)
+                {
+                    var form = new ProductForm();
+                }
+                
+            }
+            else if (typeof(T) == typeof(Seller))
+            {
+
+            }
+            else if (typeof(T) == typeof(Customer))
+            {
+
+            }
         }
     }
 }
